fc80945da3501ec016ba409ca17977ab8a45689c
T Y <>
2020-10-10 08:54:33 +0900

    no message

--- app/controllers/boards__controller.rb
+++ app/controllers/boards__controller.rb
@@ -1,13 +1,13 @@
-class BoardsController < ApplicationController
-    def index
-        @boards = Board.all
-    end
+# class BoardsController < ApplicationController
+#     def index
+#         @boards = Board.all
+#     end
 
-    def show
-        @board = Board.find(params[:id])
-    end
+#     def show
+#         @board = Board.find(params[:id])
+#     end
 
-    def new
-        @board = Board.new
-    end
-end
+#     def new
+#         @board = Board.new
+#     end
+# end
