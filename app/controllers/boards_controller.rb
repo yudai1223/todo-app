93b04dfb88a6f5379e7986bf303cb9b674650125
T Y <>
2020-10-10 02:17:11 +0900

    boardモデル消えたので作成

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,13 @@
+class BoardsController < ApplicationController
+    def index
+        @boards = Board.all
+    end
+
+    def show
+        @board = Board.find(params[:id])
+    end
+
+    def new
+        @board = Board.new
+    end
+end
---
+++ db/migrate/20201009161241_create_boards.rb
@@ -0,0 +1,10 @@
+class CreateBoards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :boards do |t|
+        t.string :title
+        t.text :content
+        t.references :user, foreign_key: true
+        t.timestamps
+    end
+  end
+end
