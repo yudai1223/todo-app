5d5dba10bc3e2b7c00f41b23f929c8844e3455ff
T Y <>
2020-10-10 10:16:28 +0900

    newboard

--- db/migrate/20201004142954_create_boards.rb
+++ db/migrate/20201004142954_create_boards.rb
@@ -1,9 +1,9 @@
 class CreateBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :boards do |t|
+      t.references :user,  null:false
       t.string :title
       t.text :content
-      t.references :user, foreign_key: true
       t.timestamps
     end
   end
