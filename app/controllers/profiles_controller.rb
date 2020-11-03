44d098ccaeee5230354fb76c7275da97ae6c626e
T Y <>
2020-11-04 01:14:30 +0900

    ユーザー画像

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,38 @@
-class ProfilesController < ApplicationCntroller
+class ProfilesController < ApplicationController
    before_action :authenticate_user!
     def show
     @profile = current_user.profile
    end
 
    def edit
+#     if current_user.profile.present?
+#         @profile = current_user.profile
+#      else
+#     @profile = current_user.build_profile
+#    end
 
+     @profile = current_user.prepare_profile
    end
-
    def update
-    
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+       redirect_to profile_path
+    else
+       render :edit 
+    end
    end
+
+   private
+   def profile_params
+     params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :subdcribed,
+      :birthday,
+      :avatar
+     )
+   end
+
 end
