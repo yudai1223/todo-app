e7fac835b5a53ad3c2ef17d6c41d8cea725cf699
T Y <>
2020-11-06 23:30:00 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'boards#index'
-  # resources :board, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-  resources :boards do
-  end
-  resources :tasks do
+  # resources :boards, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+  resources :boards  do
+  resources :tasks
   end
 
+  resource :profile, only: [:show, :edit, :update] do
 end
-
-
+end
