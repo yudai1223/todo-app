7a0b4aa20ce36c136ba4eabdaaf33c3b4a77c2a4
T Y <>
2020-11-12 00:22:27 +0900

    tasks/new エラー

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,12 +5,13 @@ class BoardsController < ApplicationController
     end
 
     def show
-        @board = Board.find (params[:id])
+        # @board = Board.find (params[:id])
+        @board = current_user.boards.find (params[:id])
     end
 
     def new
         # @boards = current_user.board.build
-        @boards = current_user.boards.build
+        @board = current_user.boards.build
         # @board = Board.new
     end
 
@@ -29,6 +30,22 @@ class BoardsController < ApplicationController
         #  end
     end
     
+    def edit
+        @boards = current_user.boards.find(params[:id])
+    end
+    
+    def update
+        @board = current_user.boards.find(params[:id])
+        @board.update(board_params)
+        redirect_to board_path(@board)
+    end
+
+    def destroy
+        board = current_user.boards.find(params[:id])
+        board.destroy!
+        redirect_to root_path
+    end
+
     end
     private
     def board_params
--- db/migrate/20201023154956_create_tasks.rb
+++ db/migrate/20201023154956_create_tasks.rb
@@ -2,9 +2,10 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.references :user,  null:false
-      t.references :bords
+      t.references :board, null:false
       t.string :title
       t.text :content
+      t.date :limit
       t.timestamps
     end
   end
