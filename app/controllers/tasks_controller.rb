cc10d65f53d3dcc74e63ff5ea298769da5fd20da
T Y <>
2020-11-20 20:24:18 +0900

    NoMethodError in Tasks#index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,15 +4,17 @@ class TasksController < ApplicationController
     def index
         @board = Board.find(params[:board_id])
         @tasks = Task.all
+         
+       
     end
 
     def show
         @board = Board.find(params[:board_id])
         @task =  Task.find(params[:id])
+        
     end
 
     def new
-
         board = Board.find(params[:board_id])
         @task = board.tasks.build
 
