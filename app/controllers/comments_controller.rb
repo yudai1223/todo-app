2960bc5ff53753171bc054eac786af8ad381a5d6
T Y <>
2020-12-12 00:25:59 +0900

    new_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
 
     private
     def comment_params
-        params.require(:comment).permit(:content)
+        params.require(:comment).permit(:content).merge(user_id: current_user.id)
     end
 end
 
